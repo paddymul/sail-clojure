2ad57e1f933605921c9596a0fd4b5b009708af36
Paddy Mullen <paddy@chartwidget.com>
2010-08-15 14:56:30 -0400

    added extra dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject sail "1.0.0-SNAPSHOT"
   :description "FIXME: write"
+  :dev-dependencies [[swank-clojure "1.2.1"]]
   :dependencies [[org.clojure/clojure "1.2.0-beta1"]
+                 [logo "1.0.0-SNAPSHOT"]
+                 [org.clojars.fyuryu/rosado.processing "1.1.0"]
                  [org.clojure/clojure-contrib "1.2.0-beta1"]])
