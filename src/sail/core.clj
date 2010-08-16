70a312e9a925a11b4c12df834f95e5d0e8ba0dff
Paddy Mullen <paddy@chartwidget.com>
2010-08-15 20:54:34 -0400

    trying to use my boat

--- src/sail/core.clj
+++ src/sail/core.clj
@@ -1,11 +1,13 @@
 (ns sail.core
-    (:use
-   ;[rosado.processing]
+  (:use
+   ;;[rosado.processing]
    [logo.macrology]
    [logo.turtle]
    [logo.turtle-prim :only [mk-turtle]]
    [logo.draw]
    [logo.core]
+   [logo.math]
+   [sail.boat :only [boat-turn mk-boat]]
    [rosado.processing :only [background-float point
                              frame-count stroke-weight
                              no-stroke stroke-float
@@ -13,11 +15,8 @@
                              ]]))
 
 
-(def turtle-b (atom (mk-turtle :position {:x 200 :y 300} :direction 100)))
 
-
-
-
+;; marks
 (defn make-mark [x y]
   (atom (mk-turtle
          :position {:x x :y y}
@@ -30,6 +29,15 @@
     (draw-turtle a-mark)))
 
 
+(def turtle-b (atom (mk-turtle :position {:x 200 :y 300} :direction 100)))
+
+(def boat-a (atom (mk-boat :destination {:x 100 :y 100}
+                           :position {:x 50 :y 50}
+                           :direction 100
+                           )))
+
+;;(def boat-a (atom (mk-turtle :position {:x 200 :y 300} :direction 100)))
+
 (defn sail-draw []
   
   (background-float  90 0 20) ;; redraws the background
@@ -39,16 +47,18 @@
   (stroke-weight 9)  ;; sets the turtle size
   (forward! turtle-b 20)
   (draw-turtle turtle-b)
+  (reset! boat-a (boat-turn ~boat-a))
+  (draw-turtle (@boat-a :turtle))
   ;;(when (> (frame-count) 100)
-    ;;(/ 1 0))
+  ;;(/ 1 0))
 
 
-)
+  )
 
 
-  (rerun-defapplet logo-play2 :title "logoemulation"
-  :size [800 800]
-  :setup setup :draw sail-draw)
+(rerun-defapplet logo-play2 :title "logoemulation"
+                 :size [800 800]
+                 :setup setup :draw sail-draw)
 
 
 
