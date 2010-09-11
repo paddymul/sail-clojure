093593cc25f093a468bdc2d92498b4f607c1dfbc
Paddy Mullen <paddy@chartwidget.com>
2010-09-11 16:44:26 -0400

    now including the units library

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :dependencies [[org.clojure/clojure "1.2.0-beta1"]
                  [clj-stacktrace "0.1.3"]
                  [logo "1.0.0-SNAPSHOT"]
+                 [units "0.2.3"]
                  [org.clojars.fyuryu/rosado.processing "1.1.0"]
                  [org.clojure/clojure-contrib "1.2.0-beta1"]])
